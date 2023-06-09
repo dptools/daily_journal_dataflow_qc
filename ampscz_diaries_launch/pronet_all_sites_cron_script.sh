d8330706366c7a9ad61a353a752638b744d1324b
ennisthemennis <mennis@mit.edu>
2023-06-09 17:59:36 -0400

    permissions update

--- ampscz_diaries_launch/pronet_all_sites_cron_script.sh
+++ ampscz_diaries_launch/pronet_all_sites_cron_script.sh
@@ -14,11 +14,12 @@ pronet_repo_path=/opt/software/daily_journal_dataflow_qc
 bash "$pronet_repo_path"/run_full_pipeline_all_sites.sh "$pronet_repo_path"/ampscz_diaries_launch/pronet_configs
 
 # at the end of the processing, make sure that all outputs have correct permissions
-chgrp -R pronet /mnt/ProNET/Lochness/PHOENIX/*/*/processed/*/phone/audio_journals
-chmod -R 770 /mnt/ProNET/Lochness/PHOENIX/*/*/processed/*/phone/audio_journals
+sudo chgrp -R pronet /mnt/ProNET/Lochness/PHOENIX/*/*/processed/*/phone/audio_journals
+sudo chmod -R 770 /mnt/ProNET/Lochness/PHOENIX/*/*/processed/*/phone/audio_journals
 # and make sure logs are readable too
-chgrp -R pronet /opt/software/daily_journal_dataflow_qc/logs
-chmod -R 770 /opt/software/daily_journal_dataflow_qc/logs
+sudo chgrp -R pronet /opt/software/daily_journal_dataflow_qc/logs
+sudo chmod -R 770 /opt/software/daily_journal_dataflow_qc/logs
+# make this go on a cron under the root account for now so it has full permissions
 
 # TODO once server-wide emails are created by the pipeline to be sent, the actual sending can occur here too
 # - different settings for Pronet versus Prescient for email sending!
