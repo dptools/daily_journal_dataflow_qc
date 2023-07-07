c438d266c5c0fd89b8573ac23949aa19bbc301ec
ennisthemennis <mennis@mit.edu>
2023-07-07 11:50:21 -0400

    test prescient launch with site ME

--- ampscz_diaries_launch/prescient_all_sites_cron_script.sh
+++ ampscz_diaries_launch/prescient_all_sites_cron_script.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-# This script is currently on cron at 9am ET on root account for Pronet
+# This script is currently on cron at 9am local time on personal account for Prescient
 # - it manages running all of the components needed for audio journals pipeline
+# (will require some migration, including to newer aggregation server once that is fully launched)
 
 ### SERVER SPECIFIC COMPONENTS
 
-# hard coded settings for Pronet server specifically to be used below - can change as needed
-server_label="ProNET Production"
-repo_path=/opt/software/daily_journal_dataflow_qc
-configs_path="$repo_path"/ampscz_diaries_launch/pronet_configs
-permissions_group_name=pronet
-data_root=/mnt/ProNET/Lochness/PHOENIX
+# hard coded settings for Prescient server specifically to be used below - can change as needed
+# note these paths are currently for the original Prescient data aggregation server 
+# (where old interview code continues to run for time being as well)
+server_label="Prescient Production"
+repo_path=/home/cho/soft/daily_journal_dataflow_qc
+configs_path="$repo_path"/ampscz_diaries_launch/prescient_configs
+permissions_group_name=prescient
+data_root=/mnt/prescient/Prescient_production/PHOENIX
+
 # part of settings are the list of addresses for two main types of email alerts
 # first list of addresses that will get high level summary email with embedded highlights
-basic_email="mennis2@mgb.org,pwolff@emory.edu,JTBAKER@mgb.org,ELIEBENTHAL@mclean.harvard.edu,linying.li@emory.edu,hrahimieichi@mgb.org,zarina.bilgrami@emory.edu,sylvain.bouix@etsmtl.ca,angela.nunez@yale.edu,SRay@northwell.edu"
+basic_email="mennis2@mgb.org,sophie.tod@orygen.org.au,eliebenthal@mclean.harvard.edu,HRAHIMIEICHI@mgb.org,jtbaker@mgb.org,sylvain.bouix@etsmtl.ca,dominic.dwyer@orygen.org.au,barnaby.nelson@orygen.org.au" 
 basic_email_from="mennis2@mgb.org" # sendmail supports easy from address specification
 # then list of addresses that will receive email with attachments that give much more detailed QC/accounting
-detailed_email="mennis2@mgb.org,linying.li@emory.edu,pwolff@emory.edu" 
-mailx_attach="A" # lookup flag that mailx command on your server uses for attachments
+detailed_email="mennis2@mgb.org,sophie.tod@orygen.org.au,HRAHIMIEICHI@mgb.org" 
+mailx_attach="a" # lookup flag that mailx command on your server uses for attachments
 # finally just a flag to determine whether server-wide emails get sent daily or only weekly on Mondays
-daily_testing_mode=0
-testing_email="mennis@g.harvard.edu" 
+daily_testing_mode=1
+testing_email="mennis2@mgb.org" 
 # both emails will go to the same address(es) when in testing mode, different than normal weekly
 
-# python env setup as well
-. /opt/software/miniconda3/etc/profile.d/conda.sh 
+# activate python environment, with setup paths specific to current Prescient server
+source ~/.bash_profile
+source /home/cho/miniconda3/etc/profile.d/conda.sh
 conda activate audio_process
+
 
 
 ### MORE GENERAL COMPONENTS
@@ -51,6 +57,4 @@ sudo chmod -R 770 "$data_root"/*/*/processed/*/phone/audio_journals
 sudo chgrp -R "$permissions_group_name" "$repo_path"/logs
 sudo chmod -R 770 "$repo_path"/logs
 # obviously this part assumes account running it is root, would have to remove suod otherwise
-
-
 
